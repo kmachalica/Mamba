5177141cdaca93b5ecbfe0f54793384705922443
kmachalica <kmachalica98@gmail.com>
2023-01-08 15:19:00 +0100

    the snake is moving and changing direction after some interval

--- app.js
+++ app.js
@@ -1,68 +1,191 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const board = document.getElementById('playground');
 
-canvas.width = 700;
-canvas.height = 500;
 
-//Pajączek
-const spider = {
-    x: 10,
-    y: 10,
 
-    draw: function()
+
+class SnakeTile{
+    constructor(x,y,velX,velY,direction,pos){
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+        this.direction = direction
+        this.div = document.createElement('div');
+        this.div.classList.add('snakeTile');
+        if(pos==1){this.div.style.borderRadius = "50% 0% 0% 50%"; this.div.style.backgroundColor = "#1ff1ff"}
+        this.div.style.left = this.x+"px";
+        this.div.style.top = this.y+"px";
+        board.appendChild(this.div)
+    }
+
+    move()
     {
-        ctx.beginPath();
-        ctx.fillStyle = "red";
-        ctx.arc(this.x,this.y,8,0,2*Math.PI);
-        ctx.fill();
-        ctx.closePath();
-    },
 
-    move: function(distanceX = 0,distanceY = 0)
+        if((this.changePositionX||this.changePositionX==0) && this.changePositionX==this.x && this.changePositionY==this.y)
+            {
+                if(this.newDirection == "u"){this.velX = 0; this.velY = -5; this.direction = "u"}
+                else if(this.newDirection == "d"){this.velX = 0; this.velY = 5; this.direction = "d"}
+                else if(this.newDirection == "l"){this.velX = -5; this.velY = 0; this.direction = "l"}
+                else if(this.newDirection =="r"){this.velX = 5; this.velY = 0; this.direction = "r"}
+               /* switch(this.newDirection)
+                {
+                    case "u": this.velX = 0; this.velY = -5; this.direction = "u"; break;
+                    case "d": this.velX = 0; this.velY = 5; this.direction = "d"; break;
+                    case "l": this.velX = -5; this.velY = 0; this.direction = "l"; break;
+                    case "r": this.velX = 5; this.velY = 0; this.direction = "r"; break;
+
+                    default: alert("SOmeting wrong"+this.changeDirection)
+                }*/
+
+                this.newDirection = null; this.changePositionX = null; this.changePositionY = null;
+            }
+        
+        else{
+            console.log(this.x,this.y,this.changePositionX,this.changePositionY,this.newDirection,this.direction)
+            if(this.x+this.velX<0||this.x+this.velX+5>650){
+                if(snake.indexOf(this)==0){
+                    if(this.y+10>450){this.direction = chooseDirection(['u'])}
+                    else if(this.y-5<0){this.direction = chooseDirection(['d'])}
+                    else {this.direction = chooseDirection(['u','d'])}
+                }
+                else{this.direction = snake[snake.indexOf(this)-1].direction}
+                
+                this.changeDirection()
+           
+            }   
+            else if(this.y+this.velY<0||this.y+this.velY+5>450)
+            {
+                if(snake.indexOf(this)===0)
+                {
+                    if(this.x-5<0){this.direction = chooseDirection(['r'])}
+                    else if(this.x+10>650){this.direction = chooseDirection(['l'])}
+                    else{this.direction = chooseDirection(['r','l'])}
+                }
+                else{this.direction = snake[snake.indexOf(this)-1].direction}
+    
+                this.changeDirection()
+            }
+        }
+        
+        
+        
+        this.x+=this.velX;
+        this.y+=this.velY;
+        this.div.style.left = this.x+"px";
+        this.div.style.top = this.y+"px";
+    }
+
+    changeDirection()
     {
-        this.x+=distanceX;
-        this.y+=distanceY; 
+        switch(this.direction)
+        {
+            case "u": this.velX = 0; this.velY = -5; break;
+            case "d": this.velX = 0; this.velY = 5; break;
+            case "r": this.velX = 5; this.velY = 0; break;
+            case "l": this.velX = -5; this.velY = 0; break;
+
+            default: alert('Something is wrong!: '+this.direction)
+        }
     }
 }
 
-
-//Wąż
-const snake = {
-    x: 250,
-    y: 250,
-    velX: 5,
-    velY: 0,
-
-    
+function chooseDirection(directions)
+{
+    const num = Math.floor(Math.random()*directions.length);
+    return directions[num]
 }
 
-
-//Functions
-
-function currentBoard(){
-    
-    setInterval(()=>{
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        ctx.fillStyle = "#999";
-        ctx.fillRect(25,25,canvas.width-50,canvas.height-50)
-        spider.draw()
-    },20)
-    
-    
-}
+const snake = []            //tile od snake tu będą trzymane
 
 
-currentBoard();
-window.onkeydown = (e)=>{
-    let distanceX,distanceY;
-    switch(e.key){
-        case "ArrowLeft": distanceX = -5; break;
-        case "ArrowRight": distanceX = 5; break;
-        case "ArrowDown": distanceY = 5; break;
-        case "ArrowUp": distanceY = -5; break;
-        default: distanceX = 0; distanceY = 0;
+
+
+function createSnake()
+{
+    for(let i = 0; i < 5; i++)
+    {
+        let newSnake = new SnakeTile(100+i*5,100,-5,0,'l',i+1);
+        snake.push(newSnake)
     }
-
-    spider.move(distanceX,distanceY)
 }
+let changeDirectionInterval;
+
+function directionInterval(...args)
+{
+    
+    if(args[0]==0){
+        
+        clearInterval(changeDirectionInterval)
+    }
+    else if(args[0]==1){
+        changeDirectionInterval = setInterval(()=>{
+            clearInterval(moveInterval)
+            let currentDirection = snake[0].direction;
+            let newDirection;
+            
+           while(checkIfItIsPossible(newDirection)){
+               console.log(currentDirection)
+            switch(currentDirection)
+            {
+                case 'd': newDirection = chooseDirection(['l','r','d']); break;
+                case 'u': newDirection = chooseDirection(['l','r','u']); break;
+                case 'l': newDirection = chooseDirection(['u','d','l']); break;
+                case 'r': newDirection = chooseDirection(['u','d','r']); break;
+
+                default: alert('Something is wrong'+ currentDirection)
+            }
+           }
+            
+            switch(newDirection){
+                case "d": snake[0].velX = 0; snake[0].velY = 5; snake[0].direction = newDirection; break;
+                case "u": snake[0].velX = 0; snake[0].velY = -5; snake[0].direction = newDirection; break;
+                case "r": snake[0].velX = 5; snake[0].velY = 0; snake[0].direction = newDirection; break;
+                case "l": snake[0].velX = -5; snake[0].velY = 0; snake[0].direction = newDirection; break;
+            }
+
+            snake.forEach((tile,index)=>{
+                if(index!=0){
+                    tile.changePositionX = snake[0].x;
+                    tile.changePositionY = snake[0].y;
+                    tile.newDirection = newDirection;
+                }
+            });
+            moveInterval =  setInterval(()=>{
+                snake.forEach(tile=>tile.move())
+            },20)
+            snake.forEach(element=>console.log(element.x,element.y,element.direction,element.newDirection))
+        },1400)
+    }
+}
+
+function checkIfItIsPossible(direction){
+    if(!direction){return true}
+    else{
+        if(snake[0].x+50 > 650 && direction == "r"){return true}
+        else if(snake[0].x -50 < 0 && direction == "l"){return true}
+        else if(snake[0].y +50 > 450 && direction == "d"){return true}
+        else if(snake[0].y - 50 < 0 && direction == "u"){return true}
+        else{return false}
+    }
+}
+
+let moveInterval =  setInterval(()=>{
+    snake.forEach(tile=>tile.move())
+},20)
+createSnake()
+/*let moveInterval = setInterval(()=>{
+    snake.forEach(tile=>tile.move())
+},20)*/
+
+directionInterval(1)
+
+let interv = setInterval(()=>{
+    if(Math.abs(snake[0].x-snake[1].x)>20||Math.abs(snake[0].y-snake[1].y)>20)
+    {
+        directionInterval(0);
+        clearInterval(moveInterval)
+    }
+},20)
+
+
 
